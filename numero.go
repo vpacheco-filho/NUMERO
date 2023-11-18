3e4c5c347053f669b554d89f51ffb77780f97524
Vanderlei Pacheco Filho <vpacheco.filho@gmail.com>
2023-11-18 17:00:18 -0300

    first commit

---
+++ numero.go
@@ -0,0 +1,11 @@
+package main //Pacote Principal//
+
+import "fmt"
+
+func main() { /*Função Principal*/
+
+	fmt.Println("Inteiros")
+	fmt.Println("1 + 1 =", 1+1, "| 1 - 1 =", 1-1, "| 1 * 2 =", 1*2, "| 4 / 2 =", 4/2, "|")
+	fmt.Println("Float")
+	fmt.Println("2.4 + 2.4 =", 2.4+2.4, "| 2.4 - 2.1 =", 2.4-2.1, "| 2.4 * 2.4 =", 2.4*2.4, "| 4.8 / 1.5 =", 4.8/1.5, "|")
+}
